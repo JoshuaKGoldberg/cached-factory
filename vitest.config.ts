8611efafafa7cfd155c6cd2bb846bc25830cca0a
Josh Goldberg <git@joshuakgoldberg.com>
2025-03-26 10:14:15 -0400

    chore: bump to create-typescript-app@2

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,9 +5,7 @@ export default defineConfig({
 		clearMocks: true,
 		coverage: {
 			all: true,
-			exclude: ["lib"],
 			include: ["src"],
-			provider: "istanbul",
 			reporter: ["html", "lcov"],
 		},
 		exclude: ["lib", "node_modules"],
