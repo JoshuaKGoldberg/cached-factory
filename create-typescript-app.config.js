3eba0c559bdd05b85110ce4261b240bcd9b58607
Josh Goldberg <git@joshuakgoldberg.com>
2025-04-04 16:48:01 -0400

    chore: bump create-typescript-app to remove its config

--- create-typescript-app.config.js
+++
@@ -1,9 +0,0 @@
-import { blockCTATransitions, createConfig } from "create-typescript-app";
-
-export default createConfig({
-	refinements: {
-		blocks: {
-			add: [blockCTATransitions],
-		},
-	},
-});
